9bbb71d4afa0f7dedcb7a390a0175d37e73ee877
Abhay Srivastav <148490972+AbhaySrivastav18@users.noreply.github.com>
2025-07-17 06:21:16 +0000

    Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,42 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) we are using node js,not browser
+
+/* 
+number => 2 to power 53
+bigint
+string
+boolean
+null => standalone value 
+undefined =>
+*/
+let age = 18;
+let isloggedIn = false;
+
+// console.log(typeof(null)); //object
+// console.log(typeof(undefined)); //undefined
+
+
+/*data conversion
+let score = "33";
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+
+"33"=> 33
+"33abc"=>NaN
+
+let booleanIn = "";
+let booleanLoggedIn = Boolean(booleanIn);
+console.log(booleanLoggedIn);
+
+1=>true 0=>false
+""=>false "abc"=>true;
+let someNumber = 33;
+let stringNumber = Number(someNumber);
+
+console.log(typeof(stringNumber));
+*/
+
+
